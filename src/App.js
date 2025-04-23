672f2c33bf96e5d53fefd3ae1de919796b812f80
Divyansh Manohar <divyanshmanohar@gmail.com>
2025-04-23 23:59:13 +0530

    Initial NeoTalent landing page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to NeoTalent</h1>
+        <p>Helping job seekers & companies find each other effortlessly.</p>
+        <button>Submit Resume</button>
+      </header>
+    </div>
+  );
+}
